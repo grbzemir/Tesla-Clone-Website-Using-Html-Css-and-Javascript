7ba238a72d3625752eb57fc50e18db6299b6f2ab
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-06-22 03:29:08 +0300

    Sixth Commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 /*javaScript kodları */
+
+const main = document.querySelector("main-1");
+
+const menu = document.querySelector("#menu");
+const cross = document.querySelector(".fa-xmark");
+const blur = document.querySelector('.blur');
+const side = document.querySelector('.hdn-side');
+
+menu.addEventListener("click", () => {
+    side.classList.add("active");
+    blur.classList.add("active");
+});
+
+cross.addEventListener("click", () => {
+    side.classList.remove("active");
+    blur.classList.remove("active");
+});
